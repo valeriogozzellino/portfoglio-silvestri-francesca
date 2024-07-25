e726282da02894f47e17f5d3fe3fab3e067cf317
valeriogozzellino <gozzellinovalerio@gmal.com>
2024-07-25 11:59:28 +0200

    changed home page

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -12,6 +12,17 @@ const Gallery = ({ dataType }) => {
 
   return (
     <div id="portfolio" className="w-full pb-[5%]">
+      <style jsx>{`
+        @media (min-width: 1024px) {
+          .hover-reveal {
+            opacity: 0; // Nascondi il testo di default per schermi grandi
+            transition: opacity 0.5s ease-in-out;
+          }
+          .group:hover .hover-reveal {
+            opacity: 100; // Mostra il testo al passaggio del mouse per schermi grandi
+          }
+        }
+      `}</style>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mb-3">
         {dataProjects.slice(0, 2).map((project, index) => (
           <div key={index} className="relative group">
@@ -20,10 +31,10 @@ const Gallery = ({ dataType }) => {
               alt={project.title}
               layout="responsive"
               width={800}
-              height={600}
+              height={700}
               className="object-cover rounded"
             />
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity bg-black bg-opacity-50">
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-2 hover-reveal">
               <div className="text-white text-center">
                 <h3 className="text-2xl font-bold">{project.title}</h3>
                 <p className="text-sm">{project.description}</p>
@@ -43,7 +54,7 @@ const Gallery = ({ dataType }) => {
               height={600}
               className="object-cover rounded"
             />
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity bg-black bg-opacity-50">
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-2 hover-reveal">
               <div className="text-white text-center">
                 <h3 className="text-2xl font-bold">{project.title}</h3>
                 <p className="text-sm">{project.description}</p>
@@ -63,7 +74,7 @@ const Gallery = ({ dataType }) => {
               height={600}
               className="object-cover rounded"
             />
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity bg-black bg-opacity-50">
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-2 hover-reveal">
               <div className="text-white text-center">
                 <h3 className="text-2xl font-bold">{project.title}</h3>
                 <p className="text-sm">{project.description}</p>
@@ -83,7 +94,7 @@ const Gallery = ({ dataType }) => {
               height={600}
               className="object-cover rounded"
             />
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity bg-black bg-opacity-50">
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-2 hover-reveal">
               <div className="text-white text-center">
                 <h3 className="text-2xl font-bold">{project.title}</h3>
                 <p className="text-sm">{project.description}</p>
