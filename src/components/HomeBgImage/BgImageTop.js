2c34e7f2bc29dcccc71a24d4320575c6f671752a
valeriogozzellino <gozzellinovalerio@gmal.com>
2024-09-08 10:55:48 +0200

    implemented responsive img

--- src/components/HomeBgImage/BgImageTop.js
+++ src/components/HomeBgImage/BgImageTop.js
@@ -19,60 +19,69 @@ const BgImageMobile = () => {
   }, []);
 
   return (
-    <div className="absolute w-full h-[120vh] inset-0 -z-[1]">
+    <div className="absolute w-full h-[100vh] inset-0 -z-[1]">
       <div
         id="imgTopLeft"
-        className="absolute top-0 left-0 flex items-center justify-center w-[600px] h-[500px]"
+        className="absolute top-0 left-0 flex items-center justify-center w-[30vw] h-[30vh] sm:w-[25vw] sm:h-[25vh] md:w-[35vw] md:h-[35vh] lg:w-[40vw] lg:h-[40vh] xl:w-[45vw] xl:h-[45vh]"
         style={{ transform: `translateY(${scrollPosition * 0.2}px)` }}>
         <Image
           src="/images/coverHome/CAPOPLAZA.jpg"
           alt="image top left"
-          width={500}
-          height={500}
+          fill
+          sizes="(max-width: 1024px) 100vw, (min-width: 1025px) 50vw"
+          className="object-contain"
         />
       </div>
       <div
         id="imgTopRight"
-        className="absolute top-0 right-0 flex items-center justify-center w-[600px] h-[500px]"
+        className="absolute top-0 right-0 flex items-center justify-center w-[30vw] h-[30vh] sm:w-[25vw] sm:h-[25vh] md:w-[35vw] md:h-[35vh] lg:w-[40vw] lg:h-[40vh] xl:w-[45vw] xl:h-[45vh]"
         style={{ transform: `translateY(${scrollPosition * 0.2}px)` }}>
         <Image
           src="/images/coverHome/LAZZA.jpg"
           alt="image top right"
-          width={500}
-          height={500}
+          fill
+          sizes="(max-width: 1024px) 100vw, (min-width: 1025px) 50vw"
+          className="object-contain"
         />
       </div>
       <div
         id="imgBottomLeft"
-        className="absolute bottom-0 left-0 flex items-center justify-center w-[500px] h-[600px]"
+        className="absolute bottom-0 left-0 flex items-center justify-center w-[30vw] h-[30vh] sm:w-[25vw] sm:h-[25vh] md:w-[35vw] md:h-[30vh] lg:w-[40vw] lg:h-[40vh] xl:w-[45vw] xl:h-[45vh]"
         style={{ transform: `translateY(-${scrollPosition * 0.2}px)` }}>
         <Image
           src="/images/coverHome/GEOLIER.jpg"
           alt="image bottom left"
-          width={500}
-          height={450}
+          fill
+          sizes="(max-width: 1024px) 100vw, (min-width: 1025px) 50vw"
+          className="object-contain"
         />
       </div>
       <div
         id="imgBottomRight"
-        className="absolute bottom-0 right-0 flex items-center justify-center w-[500px] h-[600px]"
+        className="absolute bottom-0 right-0 flex items-center justify-center w-[30vw] h-[30vh] sm:w-[25vw] sm:h-[25vh] md:w-[35vw] md:h-[30vh] lg:w-[40vw] lg:h-[40vh] xl:w-[45vw] xl:h-[45vh]"
         style={{ transform: `translateY(-${scrollPosition * 0.2}px)` }}>
         <Image
           src="/images/coverHome/NIKYSAVAGE.jpg"
           alt="image bottom right"
-          width={500}
-          height={40}
+          fill
+          sizes="(max-width: 1024px) 100vw, (min-width: 1025px) 50vw"
+          className="object-contain"
         />
       </div>
       <div
         id="imgBottomCenter"
-        className="absolute  bottom-20 right-[420px] flex items-center justify-center w-[500px] h-[500px] "
-        style={{ transform: `translateY(${scrollPosition * 0.05}px)` }}>
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-[30vw] h-[30vh] sm:w-[25vw] sm:h-[25vh] md:w-[35vw] md:h-[35vh] lg:w-[40vw] lg:h-[40vh] xl:w-[45vw] xl:h-[45vh]"
+        style={{
+          transform: `translate(-50%, -50%) translateY(${
+            scrollPosition * 0.3
+          }px)`,
+        }}>
         <Image
           src="/images/coverHome/EMIS.png"
-          alt="image bottom right"
-          width={400}
-          height={250}
+          alt="image bottom center"
+          fill
+          sizes="(max-width: 1024px) 100vw, (min-width: 1025px) 50vw"
+          className="object-contain"
         />
       </div>
     </div>
